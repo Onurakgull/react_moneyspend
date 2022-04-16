522c84756a36322de6908a1890da2dc4613ffbfa
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2022-04-16 14:07:25 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import Header from './components/Header';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Product from './components/Product';
 import product from './product.json'
 
 
 function App() {
   const [money, setMoney]=useState(100);
+  const [basket, setBasket]=useState([]);
+
+  useEffect(()=>{
+    console.log(basket)
+  },[basket])
   return (
     <>
     <div>
      <Header money={money}/>
      {product.map(product => (
-       <Product product={ product }/>
+       <Product product={ product } basket={basket} setBasket={setBasket}/>
      ))} 
     </div>
     </>
