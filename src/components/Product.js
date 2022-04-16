e3ffe471168bc0b0a6cb7be14b5bd0b3550aab16
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2022-04-16 14:07:19 +0300

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
-const Product = ( { product }) => {
+const Product = ( { product, basket, setBasket }) => {
+  const addBasket= ()=>{
+    setBasket([...basket, product])
+  }
+
   return (
-    <div>
-        <ul>
-            <li>
-            { product.title }
-            </li>
-        </ul>
+    <div className='product'>
+      <h6>{ product.title }</h6>
+      <div className='price'>$ { product.price }</div>
+      <div className='actions'>
+        <button onClick={addBasket}>Satın Al</button>
+        <span className='amount'>0</span>
+        <button>Sat</button>
+      </div>
+      <style jsx>{`
+        .product{
+          padding:10px;
+          background:#fff;
+          border: 1px solid #ddd
+        }
+      `}
+      </style>
     </div>
   )
 }
