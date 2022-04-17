30208cb8bf499a4515067e1f8510600600fc78c1
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2022-04-17 16:35:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import Header from './components/Header';
 import {useState, useEffect} from 'react';
 import Product from './components/Product';
 import product from './product.json'
+import Basket from './components/Basket';
 
 
 function App() {
   const [money, setMoney]=useState(100);
   const [basket, setBasket]=useState([]);
+  const [total, setTotal] = useState(0)
 
   useEffect(()=>{
-    console.log(basket)
+    setTotal(basket.reduce((acc, item) => {
+        return acc + (item.amount * (product.find(product => product.id === item.id ).price))
+      },0)
+    )
   },[basket])
+
+  const resetButton=()=>{
+    setBasket([])
+  }
   return (
     <>
-    <div>
-     <Header money={money}/>
+   
+     <Header total={total} money={money}/>
      {product.map(product => (
-       <Product product={ product } basket={basket} setBasket={setBasket}/>
-     ))} 
-    </div>
+       <Product total={total} money={money} key={product.id} product={ product } basket={basket} setBasket={setBasket}/>
+     ))}     
+     {total > 0 && (
+     <Basket resetButton={resetButton} product={product} basket={ basket } total={total}/>
+     )}
+
     </>
   );
 }
