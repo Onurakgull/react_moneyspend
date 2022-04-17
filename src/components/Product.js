483a0b834d2b1f0cc6e5cb9ed68ac9478720fc2a
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2022-04-17 16:35:57 +0300

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const Product = ( { product, basket, setBasket }) => {
+const Product = ( { product, money, total, basket, setBasket }) => {
+
+  const basketItem = basket.find(item => item.id === product.id)
+
   const addBasket= ()=>{
-    setBasket([...basket, product])
+   const checkBasket = basket.find(item => item.id === product.id)
+    //Ürün daha önce eklenmişse,
+   if(checkBasket){
+    checkBasket.amount += 1
+    setBasket([ ...basket.filter(item => item.id !== product.id), checkBasket])
+   }else{
+      setBasket([...basket , {
+        id : product.id,
+        amount : 1
+      }])
+   }
+  }
+  const removeBasket =() => {
+    const checkBasket = basket.find(item => item.id === product.id)
+    const removeWithoutCurrent = basket.filter(item => item.id !== product.id)
+
+    checkBasket.amount -= 1
+    if(checkBasket.amount === 0){
+      setBasket([ ...removeWithoutCurrent])
+    }else{
+      setBasket([...removeWithoutCurrent, checkBasket])
+    }
+   
   }
 
   return (
@@ -10,9 +35,9 @@ const Product = ( { product, basket, setBasket }) => {
       <h6>{ product.title }</h6>
       <div className='price'>$ { product.price }</div>
       <div className='actions'>
-        <button onClick={addBasket}>Satın Al</button>
-        <span className='amount'>0</span>
-        <button>Sat</button>
+        <button disabled={total + product.price > money} onClick={addBasket}>Satın Al</button>
+        <span className='amount'>{ basketItem && basketItem.amount || 0 }</span>
+        <button disabled={!basketItem} onClick={removeBasket}>Sat</button>
       </div>
       <style jsx>{`
         .product{
