cc84cca25feb8acf0c04feef6a83e9f6614b39dd
onurakgl <73547559+Onurakgull@users.noreply.github.com>
2022-04-18 16:10:02 +0300

    Update Basket.js

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import BasketItem from './BasketItem'
+import { moneyFormat } from '../helpers'
+import './Product.css'
 
 const Basket = ( { basket, product, total, resetButton } ) => {
   return (
     <>
+       <div className='basket-container container'>
+        <h3>Alışveriş Detayları</h3>
+        <ul>
         {basket.map(item => (
             <BasketItem item={item} product={product.find(p => p.id === item.id)}/>
         ))}
-        Toplam Satış= ${total}
+        </ul>
+         <div className='total'>
+            Toplam Satış= ${moneyFormat(total)}
+        </div>
         <br/>
-        <button onClick={resetButton}>Sepeti Sıfırla</button>
+        <button className='reset-btn' onClick={resetButton}>Sepeti Sıfırla</button>
+       </div>
     </>
   )
 }
